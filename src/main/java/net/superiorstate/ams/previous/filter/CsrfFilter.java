d10aa9a99d5cc7c552047b6e39e01af9e0a85a5c
KevinMurphy <kmurp55@student.wgu.edu>
2026-02-13 12:03:13 -0600

    Fix security: Register CsrfFilter with @WebFilter annotation

--- src/main/java/net/superiorstate/ams/previous/filter/CsrfFilter.java
+++ src/main/java/net/superiorstate/ams/previous/filter/CsrfFilter.java
@@ -1,10 +1,11 @@
 package net.superiorstate.ams.previous.filter;
 
 import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
 import jakarta.servlet.http.*;
 import java.io.IOException;
 import java.util.UUID;
-
+@WebFilter("/*")
 public class CsrfFilter implements Filter {
     @Override
     public void doFilter(ServletRequest req, ServletResponse resp, FilterChain chain)
